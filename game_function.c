3064e0caa4dd36661c911c964ca8fedd100a47ba
Guillaume B <105287640+Auxifruit@users.noreply.github.com>
2023-12-17 00:06:40 +0100

    Update game_function.c

--- game_function.c
+++ game_function.c
@@ -1,67 +1,90 @@
-#include "game_function.h"
+#include "game.h"
 
-/*! @brief Function to initialize the game */
-Game* init_game() {
-    Game* match = NULL;                                 // Locals
-    
-    match = malloc(sizeof(Game));
-    if(match == NULL) {                                 // Allocation check 
+/*! @brief Function to initialize a game */
+Grid* init_grid() {
+    Grid* grid = NULL;                                  // Locals
+
+    grid = malloc(sizeof(Grid));
+    if(grid == NULL) {                                  // Allocation check 
         printf("Allocation error (init_grid).\n");
         exit(ALLOCATION_ERROR);
     }
 
-    match->player_grid = init_grid();
-    match->player_boat = init_boat();
+    grid->size = S_GRID;
 
-    match->computer_grid = init_grid();
+    grid->matrix = malloc(sizeof(int*) * S_GRID);       // Allocate memory for the "rows" 
+    if(grid->matrix == NULL) {                          // Allocation check 
+        printf("Allocation error (init_grid).\n");
+        exit(ALLOCATION_ERROR);
+    }
 
-    return match;
+    for(int i = 0; i < S_GRID; i++) {
+        grid->matrix[i] = malloc(sizeof(int) * S_GRID); // Allocate memory for the "Columns" 
+        if(grid->matrix[i] == NULL) {                   // Allocation check 
+            printf("Allocation error (init_grid).\n");
+            exit(ALLOCATION_ERROR);
+        }
+    }
+
+    for(int i = 0; i < S_GRID; i++) {
+        for(int j = 0; j < S_GRID; j++) {
+            grid->matrix[i][j] = WATER;                 // Default value for each cell 
+        }
+    }
+
+    return grid;
 }
 
-/*! @brief Function to shoot on a cell */
-Grid* shoot(Grid* grid) {
-    int row;                                            // Locals
-    int col;
-
-    if(grid == NULL) {                                  // Argument verification
-        printf("Argument error (shoot).\n");
+/*! @brief Function to display a grid */
+void display_grid(Grid* grid, int person) {
+    if(grid == NULL || (person != 1 && person != 0)) { // Argument check                            // Allocation check 
+        printf("Argument error (display_grid).\n");
         exit(ARGUMENT_ERROR);
     }
 
-    do {                                                // The player input the cell he want to shoot
-        printf("Choose a ROW between 1 and 10 : ");
-        if(scanf("%d", &row) != 1) {
-            printf("Error scanf (shoot).\n");
-            exit(SCANF_ERROR);
-        }
-
-        printf("\n");
-        printf("Choose a COLUMN between 1 and 10 : ");
-        if(scanf("%d", &col) != 1) {
-            printf("Error scanf (shoot).\n");
-            exit(SCANF_ERROR);
-        }
-
-        if((row < 0 || row > 10) || (col < 0 || col > 10)) {
-            printf("You input the wrong coordinates. Please try again.\n\n");
-        }
-        else if(grid->matrix[row][col] != WATER && grid->matrix[row][col] != BOAT) {
-            printf("The cell you want to shoot is already touched. Please try again.\n\n");
-        }
-        
-    } while ((row < 0 || row > 10) || (col < 0 || col > 10) || (grid->matrix[row][col] != WATER && grid->matrix[row][col] != BOAT));    // Verification if the cell can be shoot
-
-    switch (grid->matrix[row][col]) {
-    case WATER:
-        grid->matrix[row][col] = WATER_SHOT;
-        return grid;
-    case BOAT:
-        grid->matrix[row][col] = BOAT;
-        // VERIFIER BATEAU
-        return grid;
-    default:
-        printf("ERROR\n");
-        exit(SCANF_ERROR);
+    printf("   ");
+    for(int n_col = 0; n_col < S_GRID; n_col++) {       // Print the column number 
+        printf("%d ", n_col + 1);
     }
+    printf("\n");
+
+    for(int row = 0; row < S_GRID; row++) {
+        printf("%2d ", row + 1);                        // Print the row number 
+        for(int col = 0; col < S_GRID; col++) {
+            if(person == 1) {                           // If it's the player's grid
+                printf("\033[0;%dm%c \033[0;37m", color_switch(grid->matrix[row][col]), grid->matrix[row][col]);    // Print the character with the right color 
+            }
+            else {                                      // If it's the computer's grid
+                if(grid->matrix[row][col] == BOAT) {    // If it's a boat cell, we don't show it
+                    printf("\033[0;%dm%c \033[0;37m", color_switch(WATER), WATER);
+                }
+                else {
+                    printf("\033[0;%dm%c \033[0;37m", color_switch(grid->matrix[row][col]), grid->matrix[row][col]);
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+/*! @brief Function to get the color of a matrix character */
+int color_switch(int nb) {
+    switch (nb) {
+    case 126:                                           // It's a WATER cell 
+        return 34;                                      // Blue color 
+
+    case 79:                                            // It's a WATER_SHOT cell 
+        return 36;                                      // Cyan color 
+
+    case 66:                                            // It's a BOAT cell 
+        return 33;                                      // Yellow color 
+
+    case 88:                                            // It's a WRECK cell 
+        return 31;                                      // Red color 
     
+    default:                                            // If the number isn't a valid one 
+        printf("Argument error (color_switch).\n");
+        exit(ARGUMENT_ERROR);
+        break;
+    }
 }
