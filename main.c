a6e2f3f3cc436cbf3c80fba67cf058a742bd55c1
Guillaume B <105287640+Auxifruit@users.noreply.github.com>
2023-12-17 00:07:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,22 +4,30 @@ int main() {
     Game* match = NULL;
     printf("HELLO INITIAL\n");
     match = init_game();
+    int end = 0;
     
     printf("\033[0;32mYour grid :\033[0;37m\n");
     display_grid(match->player_grid, 1);
 
+    // printf("\n\033[0;35mComputer's grid :\033[0;37m\n");
+    // display_grid(match->computer_grid, 0);
 
-    printf("\n\033[0;35mComputer's grid :\033[0;37m\n");
-    display_grid(match->computer_grid, 0);
+    for(int i = 0; i < N_BOAT; i++) {
+        match->player_boat[i] = create_boat();
+        match->computer_boat[i] = match->player_boat[i];
+    }
 
-    /*
-    match->player_grid = shoot(match->computer_grid);
+    match = place_boat(match);
 
-    printf("\n\033[0;35mComputer's grid :\033[0;37m\n");
-    display_grid(match->player_grid, 0);
-    */
+    do {
+        match->player_grid = shoot(match->player_grid);
+        printf("\033[0;32mYour grid :\033[0;37m\n");
+        display_grid(match->player_grid, 1);
+        end = verif_winner(match->player_grid);
+    } while(end == 0);
 
     printf("HELLO FINAL\n");
+    
     return 0;
 }
 
