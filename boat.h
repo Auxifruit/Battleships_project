a9bfabc5546a7cdcc9a984a4a8f43e6281cfe15a
Guillaume B <105287640+Auxifruit@users.noreply.github.com>
2023-12-17 00:06:00 +0100

    Update boat.h

--- boat.h
+++ boat.h
@@ -1,5 +1,5 @@
 #ifndef BOAT_h
-    #define BOAT_T 
+    #define BOAT_T      /*! To prevent circular inclusion */
     
         /*! @brief Structure to store the boat information */
         typedef struct {
@@ -7,11 +7,13 @@
             int** position;
             /*! Boat's size */
             int size;
-            /*! Boat's orientation (V for vertical and H for Horizontale) */
+            /*! Boat's orientation (V for vertical and H for Horizontal) */
             char orientation;               
         } Boat ;
 
         /*! @brief Function to initialize the boats */
-        Boat* init_boat();        
-
+        Boat* init_boat();
+        /*! @brief Function to create a boat */
+        Boat create_boat();
+        
 #endif
