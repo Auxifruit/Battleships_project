d5780dba4b31286f2efa5ab09779f80c3fc0c322
Guillaume B <105287640+Auxifruit@users.noreply.github.com>
2023-12-17 00:06:15 +0100

    Update boat_function.c

--- boat_function.c
+++ boat_function.c
@@ -1,4 +1,4 @@
-#include "game_function.h"
+#include "game.h"
 
 /*! @brief Function to initialize the boats */
 Boat* init_boat() {
@@ -11,4 +11,24 @@ Boat* init_boat() {
     }
     
     return boats;
+}
+
+/*! @brief Function to place boats*/
+Boat create_boat() {
+    Boat boat;                                          // Locals
+    int size;
+
+    do {                                                // For each boat we get its size
+        printf("Enter the boat's size between 1 and %d : ", S_GRID);
+        if(scanf("%d", &size) != 1) {                   // Scanf check 
+            printf("Scanf error (create_boat).\n");
+            exit(SCANF_ERROR);
+        }
+        if (size < 0 || size > S_GRID) {
+            printf("The boat's size isn't correct. Please try again.\n");
+        }
+    } while(size < 0 || size > S_GRID);
+    boat.size = size;
+    
+    return boat;
 }
