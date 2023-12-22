8b8af95f1d09a4445f088a0d4a3dcac010fa7e42
Guillaume B <105287640+Auxifruit@users.noreply.github.com>
2023-12-22 20:54:41 +0100

    Update grid.h

--- grid.h
+++ grid.h
@@ -10,10 +10,21 @@
         } Grid ;
 
         /*! @brief Function to initialize a grid */   
-        Grid* init_grid();                
+        Grid* init_grid();              
+
         /*! @brief Function to display a grid */
-        void display_grid(Grid* Grid, int person);      
+        void display_grid(Grid* Grid, int person);    
+
         /*! @brief Function to get the color of a matrix character */
         int color_switch(int nb);
+        
+        /*! @brief Function to let the player shoot a cell */
+        Grid* player_shooting(Grid* grid);
+
+        /*! @brief Function to let the computer shoot a cell */
+        Grid* computer_shooting(Grid* grid);
+
+        /*! @brief Function to check if all the boat are destroyed in a grid */
+        int verif_winner(Grid* grid);
 
 #endif
