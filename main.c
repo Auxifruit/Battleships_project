67047f254fede5032f1e6046a09333fc6fc1f65b
Guillaume B <105287640+Auxifruit@users.noreply.github.com>
2023-12-23 18:35:19 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
 #include "game.h"
 
 int main() {
+    //Locals
     Game* match = NULL;
     int end = 0;
 
-    //welcome();
+    welcome();                                                              // Welcome message
+
+    match = init_game();                                                    // Initialization of the game (boat and grid)
+
+    match = create_navy(match);                                             // We create all the boat
+
+    match = place_boat_player(match);                                       // The player places his boats
+    match = place_boat_computer(match);                                     // The computer places its boats
 
 
-    match = init_game();
-
-    match = create_navy(match);
-
-    match = place_boat_player(match);
-    match = place_boat_computer(match);
-
-
+    // We show the player's grid with all his boat
     printf("\nHere's your final grid :\n");
     display_grid(match->player_grid, 1);
     sleep(1);
@@ -26,30 +27,42 @@ int main() {
         printf("\nIt's your time to shoot !\n");
         sleep(1);
 
-        boat_remaining(match);
+        boat_remaining(match);                                              // We show the remaining boats
 
-        display_grid(match->computer_grid, 0);
+        // The player shoot
+        display_grid(match->computer_grid, 1);
         match->computer_grid = player_shooting(match->computer_grid);
-        display_grid(match->computer_grid, 0);
+        display_grid(match->computer_grid, 1);
+        sleep(2);
 
-        printf("\nBe carefull, the computer is shooting !\n");
-        sleep(1);
+        end = verif_winner(match->computer_grid, 0);                        // We verify the player won
+
+        // If we don't have a winner yet
+        if(end == 0) {
+            printf("\nBe carefull, the computer is shooting !\n");
+            sleep(1);
+            
+            // The computer shoot
+            match->player_grid = computer_shooting(match->player_grid);
+            display_grid(match->player_grid, 1);
+            sleep(2);
         
-        match->player_grid = computer_shooting(match->player_grid);
-        display_grid(match->player_grid, 1);
-        sleep(1);
-
-        end = verif_winner(match->player_grid, 1);
-        end = verif_winner(match->computer_grid, 0);
+            end = verif_winner(match->player_grid, 1);                      // We verify the computer won
+        }
+        
     } while(end == 0);
     
+    // If the computer won
     if(end == 1) {
         printf("\nYou lost, better luck next time !\n");
     }
+    // If the player won
     else {
         printf("\nYou won, you're the best commander !\n");
     }
     
+    free_all(match);
+
     printf("\nTHANK YOU FOR PLAYING !\n");
     
     return 0;
